3e6e277b13ab3ec93e70fbf6dccf20534c9c262f
Mukesh Kumar <poddarm85@gmail.com>
2020-09-28 02:08:49 +0530

    delete static files

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -120,12 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles'),
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATIC_URL = '/static/'
+# STATIC_ROOT=os.path.join(BASE_DIR,'static/')
 # Activate Django-Heroku
 # django_heroku.settings(locals())
