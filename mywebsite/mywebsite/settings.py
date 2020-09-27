23d0fad3eaa9c431397fc2315a8791bb5138f90f
Mukesh Kumar <poddarm85@gmail.com>
2020-09-28 02:30:49 +0530

    changes in setting file

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -120,7 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT=os.path.join(BASE_DIR,'static/')
-# Activate Django-Heroku
-# django_heroku.settings(locals())
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
